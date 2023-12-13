e6e8fe6725a5908644816a87b5d50eb66259eb48
Kristian Kjærgård <kris6673@hotmail.com>
2023-12-13 19:54:36 +0100

    Set some disable standards to be enable instead

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableUserSiteCreate.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableUserSiteCreate.ps1
@@ -6,11 +6,12 @@ function Invoke-CIPPStandardDisableUserSiteCreate {
     param($Tenant, $Settings)
     If ($Settings.remediate) {
         try {
-            $body = '{"isSiteCreationEnabled": false}'
+            $body = '{"isSiteCreationEnabled": true}'
             New-GraphPostRequest -tenantid $tenant -Uri 'https://graph.microsoft.com/beta/admin/sharepoint/settings' -AsApp $true -Type patch -Body $body -ContentType 'application/json'
-            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Disabled standard users from creating sites' -sev Info
-        } catch {
-            Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to disable standard users from creating sites: $($_.exception.message)" -sev Error
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Enabled standard users from creating sites' -sev Info
+        }
+        catch {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to enable standard users from creating sites: $($_.exception.message)" -sev Error
         }
     }
     if ($Settings.alert) {
@@ -18,7 +19,8 @@ function Invoke-CIPPStandardDisableUserSiteCreate {
         $CurrentInfo = New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/admin/sharepoint/settings' -tenantid $Tenant -AsApp $true
         if ($CurrentInfo.isSiteCreationEnabled -eq $false) {
             Write-LogMessage -API 'Standards' -tenant $tenant -message 'Standard users are not allowed to create sites' -sev Info
-        } else {
+        }
+        else {
             Write-LogMessage -API 'Standards' -tenant $tenant -message 'Standard users are allowed to create sites' -sev Alert
         }
     }
