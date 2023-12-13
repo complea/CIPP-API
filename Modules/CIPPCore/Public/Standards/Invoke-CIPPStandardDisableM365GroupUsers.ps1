9d7209cb3878f219b1d1be3c07f36ec227286319
Kristian Kjærgård <31723128+kris6673@users.noreply.github.com>
2023-12-13 20:31:26 +0100

    Revert "Set some disable standards to be enable instead"

--- Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableM365GroupUsers.ps1
+++ Modules/CIPPCore/Public/Standards/Invoke-CIPPStandardDisableM365GroupUsers.ps1
@@ -14,13 +14,12 @@ function Invoke-CIPPStandardDisableM365GroupUsers {
         (New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/settings/$($CurrentState.id)" -Type POST -Body $CurrentState -ContentType 'application/json')
                 $CurrentState = (New-GraphGetRequest -Uri 'https://graph.microsoft.com/beta/settings' -tenantid $tenant) | Where-Object -Property displayname -EQ 'Group.unified'
             }
-    ($CurrentState.values | Where-Object { $_.name -eq 'EnableGroupCreation' }).value = 'true'
+    ($CurrentState.values | Where-Object { $_.name -eq 'EnableGroupCreation' }).value = 'false'
             $body = "{values : $($CurrentState.values | ConvertTo-Json -Compress)}"
     (New-GraphPostRequest -tenantid $tenant -Uri "https://graph.microsoft.com/beta/settings/$($CurrentState.id)" -Type patch -Body $body -ContentType 'application/json')
-            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Standards API: Enabled users from creating M365 Groups.' -sev Info
-        }
-        catch {
-            Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to enable users from creating M365 Groups: $($_.exception.message)" -sev 'Error'
+            Write-LogMessage -API 'Standards' -tenant $tenant -message 'Standards API: Disabled users from creating M365 Groups.' -sev Info
+        } catch {
+            Write-LogMessage -API 'Standards' -tenant $tenant -message "Failed to disable users from creating M365 Groups: $($_.exception.message)" -sev 'Error'
         }
     }
     if ($Settings.alert) {
@@ -28,12 +27,10 @@ function Invoke-CIPPStandardDisableM365GroupUsers {
         if ($CurrentState) {
             if (($CurrentState.values | Where-Object { $_.name -eq 'EnableGroupCreation' }).value -eq 'false') {
                 Write-LogMessage -API 'Standards' -tenant $tenant -message 'Users are disabled from creating M365 Groups.' -sev Info
-            }
-            else {
+            } else {
                 Write-LogMessage -API 'Standards' -tenant $tenant -message 'Users are not disabled from creating M365 Groups.' -sev Alert
             }
-        }
-        else {
+        } else {
             Write-LogMessage -API 'Standards' -tenant $tenant -message 'Users are not disabled from creating M365 Groups.' -sev Alert
         }
     }
@@ -41,12 +38,10 @@ function Invoke-CIPPStandardDisableM365GroupUsers {
         if ($CurrentState) {
             if (($CurrentState.values | Where-Object { $_.name -eq 'EnableGroupCreation' }).value -eq 'false') {
                 $CurrentState = $true
-            }
-            else {
+            } else {
                 $CurrentState = $false
             }
-        }
-        else {
+        } else {
             $CurrentState = $false
         }
         Add-CIPPBPAField -FieldName 'DisableM365GroupUsers' -FieldValue [bool]$CurrentState -StoreAs bool -Tenant $tenant
